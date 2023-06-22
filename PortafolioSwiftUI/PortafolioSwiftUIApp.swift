f9fb6a51362d8955b090344951276f70b213fe56
Hugo Villagra <hasvbogarin92@hotmail.com>
2023-06-22 15:42:24 -0400

    Initial Commit

---
+++ PortafolioSwiftUI/PortafolioSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortafolioSwiftUIApp.swift
+//  PortafolioSwiftUI
+//
+//  Created by Hugo Villagra on 2023-06-22.
+//
+
+import SwiftUI
+
+@main
+struct PortafolioSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
