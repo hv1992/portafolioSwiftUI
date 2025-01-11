6a1397ddb3a7c9c00e2fd1239e5f09c920f438e3
Hugo Villagra <33612966+hv1992@users.noreply.github.com>
2025-01-10 22:58:13 -0300

    Se agrega firebase al proyecto

--- PortafolioSwiftUI/AppDelegate.swift
+++ PortafolioSwiftUI/AppDelegate.swift
@@ -6,11 +6,15 @@
 //
 
 import UIKit
+import FirebaseCore
 
 class AppDelegate : NSObject,UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         
         print("didFinishLaunchingWithOptions: AppDelegate")
+        
+        //Configurar el Firebase
+        FirebaseApp.configure()
         return true
     }
 }
